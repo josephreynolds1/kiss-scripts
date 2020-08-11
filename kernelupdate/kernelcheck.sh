96784d1d82b4567f7cadcf4b2734b30ac732400c
josephreynolds1 <46980014+josephreynolds1@users.noreply.github.com>
2020-08-11 14:07:56 -0400

    Added logging functions and related variables to scripts

--- kernelupdate/kernelcheck.sh
+++ kernelupdate/kernelcheck.sh
@@ -2,6 +2,41 @@
 
 scriptversion="1.0"
 
+### Set time variable for logging
+
+time=$(date '+%Y-%m-%d-%H:%M')
+export time
+
+### Set color variables
+
+export lcol='\033[1;33m'
+export lcol2='\033[1;36m'
+export lclr='\033[m'
+
+### Functions
+
+log() {
+    # Print a message prettily.
+    #
+    # All messages are printed to stderr to allow the user to hide build
+    # output which is the only thing printed to stdout.
+    #
+    # The l<word> variables contain escape sequence which are defined
+    # when '$KISS_COLOR' is equal to '1'.
+    printf '%b%s %b%s%b %s\n' \
+        "$lcol" "${3:-->}" "${lclr}${2:+$lcol2}" "$1" "$lclr" "$2" >&2
+}
+
+war() {
+    log "$1" "$2" "${3:-WARNING}"
+}
+
+die() {
+    log "$1" "$2" "${3:-ERROR}"
+    exit 1
+}
+
+
 ### Get current kernel version
 
 installedkernel=$(uname -r)
@@ -10,34 +45,34 @@ echo ""
 
 if [ -f /boot/kernelprevious ]
 then
-    
+
     kernelprevious=$(cat /boot/kernelprevious)
-    
-    echo "Kernelprevious file set value: $kernelprevious"
-    
+
+    log "Kernelprevious file set value:" "$kernelprevious"
+
 else
-    
-    echo "/boot/kernelprevious file not found"
-    
+
+    log "Kernelprevious file not found"
+
 fi
 
 if [ -f /boot/kernelcurrent ]
 then
-    
+
     kernelcurrent=$(cat /boot/kernelcurrent)
-    
-    echo "Kernelcurrent file set value: $kernelcurrent"
-    
+
+    log "Kernelcurrent file set value:" "$kernelcurrent"
+
 else
-    
-    echo "/boot/kernelcurrent file not found"
-    
+
+    log "Kernelcurrent file not found"
+
 fi
 
 
 
 echo ""
-echo "Installed Linux kernel: $installedkernel"
+log "Installed Linux kernel:" "$installedkernel"
 
 
 ### Get latest kernel.org stable version
@@ -49,21 +84,21 @@ latestkernel=${latestkernel##*.tar.xz\">}
 latestkernel=${latestkernel%</a>}
 
 echo ""
-echo "Stable version from Kernel.org: $latestkernel"
+log "Stable version from Kernel.org:" "$latestkernel"
 
 if [ "$latestkernel" = "$installedkernel" ]
 
 then
-    
+
     echo ""
-    echo "Kernel is already latest version"
+    log "Kernel is already latest version"
     echo ""
-    
+
 else
-    
+
     echo ""
-    echo "Newer kernel available!"
-    echo "Run KernelUpdate.sh to update the Linux kernel"
+    log "Newer kernel available!"
+    log "Run KernelUpdate.sh to update the Linux kernel"
     echo ""
-    
+
 fi
--- kernelupdate/kernelupdate.sh
+++ kernelupdate/kernelupdate.sh
@@ -2,6 +2,40 @@
 
 scriptversion="1.0"
 
+### Set time variable for logging
+
+time=$(date '+%Y-%m-%d-%H:%M')
+export time
+
+### Set color variables
+
+export lcol='\033[1;33m'
+export lcol2='\033[1;36m'
+export lclr='\033[m'
+
+### Functions
+
+log() {
+    # Print a message prettily.
+    #
+    # All messages are printed to stderr to allow the user to hide build
+    # output which is the only thing printed to stdout.
+    #
+    # The l<word> variables contain escape sequence which are defined
+    # when '$KISS_COLOR' is equal to '1'.
+    printf '%b%s %b%s%b %s\n' \
+        "$lcol" "${3:-->}" "${lclr}${2:+$lcol2}" "$1" "$lclr" "$2" >&2
+}
+
+war() {
+    log "$1" "$2" "${3:-WARNING}"
+}
+
+die() {
+    log "$1" "$2" "${3:-ERROR}"
+    exit 1
+}
+
 ### Get current kernel version
 
 installedkernel=$(uname -r)
@@ -10,34 +44,34 @@ echo ""
 
 if [ -f /boot/kernelprevious ]
 then
-    
+
     kernelprevious=$(cat /boot/kernelprevious)
-    
-    echo "Kernelprevious file set value: $kernelprevious"
-    
+
+    log "Kernelprevious file set value:" "$kernelprevious"
+
 else
-    
-    echo "/boot/kernelprevious file not found"
+
+    log "Kernelprevious file not found"
     touch "/boot/kernelprevious"
-    
+
 fi
 
 if [ -f /boot/kernelcurrent ]
 then
-    
+
     kernelcurrent=$(cat /boot/kernelcurrent)
-    
-    echo "Kernelcurrent file set value: $kernelcurrent"
-    
+
+    log "Kernelcurrent file set value:" "$kernelcurrent"
+
 else
-    
-    echo "/boot/kernelcurrent file not found"
+
+    log "Kernelcurrent file not found"
     touch "/boot/kernelcurrent"
-    
+
 fi
 
 echo ""
-echo "Installed Linux kernel: $installedkernel"
+log "Installed Linux kernel:" "$installedkernel"
 
 
 ### Get latest kernel.org stable version
@@ -48,120 +82,120 @@ latestkernel=${latestkernel##*.tar.xz\">}
 
 latestkernel=${latestkernel%</a>}
 
-echo "Stable version from Kernel.org: $latestkernel"
+log "Stable version from Kernel.org:" "$latestkernel"
 
 if [ "$latestkernel" = "$installedkernel" ]
 
 then
-    
+
     echo ""
-    echo "Kernel is already latest version"
+    log "Kernel is already latest version"
     echo ""
-    
+
 else
-    
+
     echo ""
-    echo "Newer kernel available!"
-    
+    log "Newer kernel available!"
+
     urlkernel="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${latestkernel}.tar.xz"
-    
+
     echo ""
     echo "Would you like to update the Linux Kernel to $latestkernel (y/n)? "
     read -r answer
-    
+
     if [ "$answer" != "${answer#[Yy]}" ] ;then
-        
-        echo Updating kernel....
-        
+
+        log "Updating kernel...."
+
         ## Download Kernel
-        
-        echo "Downloading kernel from: ${urlkernel}"
-        
+
+        log "Downloading kernel from:" "${urlkernel}"
+
         wget -P /usr/src/kernel/ "$urlkernel"
-        
+
         ### Extract and remove downloaded kernel archive
-        
+
         tar xvf /usr/src/kernel/linux-"${latestkernel}".tar.xz --directory /usr/src/kernel/
-        
+
         rm -rf /usr/src/kernel/linux-"${latestkernel}".tar.xz
-        
+
         ### Copy kernel config to source directory
-        
+
         cp /usr/src/kernel/config /usr/src/kernel/linux-"${latestkernel}"/.config
-        
+
         cd /usr/src/kernel/linux-"${latestkernel}"
-        
+
         ### Prepare config file
-        
+
         make olddefconfig
-        
+
         ### Compile the kernel
-        
+
         make -j "$(nproc)"
-        
+
         make INSTALL_MOD_STRIP=1 modules_install
-        
+
         make install
-        
+
         ### Rename kernel image and map file to version
-        
+
         mv /boot/vmlinuz /boot/vmlinuz-"${latestkernel}"
-        
+
         mv /boot/System.map /boot/System.map-"${latestkernel}"
-        
+
         ### Update kernelprevious/current files under /boot
-        
+
         echo "$installedkernel" > /boot/kernelprevious
         echo "$latestkernel" > /boot/kernelcurrent
-        
+
         ### Remove previous kernel files
-        
+
         if [ -f "/boot/System.map-${kernelprevious}" ]
         then
-            
+
             rm "/boot/System.map-${kernelprevious}"
-            
-            echo "Deleting.... System.map-$kernelprevious"
-            
+
+            log "Deleting...." "System.map-$kernelprevious"
+
         else
-            
-            echo "Previous System.map not found: System.map-$kernelprevious"
-            
+
+            log "Previous System.map not found:" "System.map-$kernelprevious"
+
         fi
-        
-        
+
+
         if [ -f "/boot/vmlinuz-${kernelprevious}" ]
         then
-            
+
             rm "/boot/vmlinuz-${kernelprevious}"
-            
-            echo "Deleting previous kernel image.... vmlinuz-$kernelprevious"
-            
+
+            log "Deleting previous kernel image...." "vmlinuz-$kernelprevious"
+
         else
-            
-            echo "Previous kernel image not found: vmlinuz-$kernelprevious"
-            
+
+            log "Previous kernel image not found:" "vmlinuz-$kernelprevious"
+
         fi
-        
-        
+
+
         if [ -d "/usr/src/kernel/linux-${kernelprevious}" ]
         then
-            
+
             rm -rf "/usr/src/kernel/linux-${kernelprevious}"
-            
-            echo "Deleting previous kernel source.... /usr/src/kernel/linux-${kernelprevious}"
-            
+
+            log "Deleting previous kernel source...." "/usr/src/kernel/linux-${kernelprevious}"
+
         else
-            
-            echo "Previous kernel source not found: /usr/src/kernel/linux-${kernelprevious}"
-            
+
+            log "Previous kernel source not found:" "/usr/src/kernel/linux-${kernelprevious}"
+
         fi
-        
-        
+
+
         ### Update grub config
-        
+
         grub-mkconfig -o /boot/grub/grub.cfg
-        
+
     fi
-    
+
 fi
