1d0eab5e80e79b75fa494e49f598b6b94e10975f
josephreynolds1 <user@ws01p.colossus.home>
2020-08-20 19:42:52 -0400

    Testing phase2.sh

--- os-bootstrap/source/phase2.sh
+++ os-bootstrap/source/phase2.sh
@@ -2,10 +2,7 @@
 
 ### Variables #################################################################
 
-### Set version variables
-
-export scriptversion="1.2"
-export kissversion="1.1"
+export KISS_PROMPT=0
 
 source ./scriptvars.sh
 
@@ -42,74 +39,6 @@ die() {
     exit 1
 }
 
-
-requiredTools()
-{
-    for tool in "$@" ; do
-
-        if which "${tool}" >/dev/null 2>&1; then
-            echo "${tool} : OK"
-        else
-
-            war "${tool} : Missing"
-
-            return 1
-        fi
-
-    done
-}
-
-
-urlTest()
-{
-    echo "testing $1"
-
-    if curl -fsS "$1" >/dev/null; then
-        echo "$1 is accessible"
-    else
-        echo "$1 is inacessible"
-    fi
-
-}
-
-
-downloadSource()
-{
-
-    wget -P "$1" "$2" || scriptFail "Failed to download: $2"
-
-}
-
-
-contains() {
-    string="$1"
-    substring="$2"
-    if test "${string#*$substring}" != "$string"
-    then
-
-        return 0    # $substring is in $string
-
-    else
-
-        return 1    # $substring is not in $string
-
-    fi
-}
-
-decompress() {
-    case $1 in
-        *.bz2)      bzip2 -d  ;;
-        *.lzma)     lzma -dc  ;;
-        *.lz)       lzip -dc  ;;
-
-        *.tar)      cat       ;;
-        *.tgz|*.gz) gzip -d   ;;
-        *.xz|*.txz) xz -dcT 0 ;;
-        *.zst)      zstd -dc  ;;
-    esac < "$1"
-}
-
-
 ### Virtual Machine check
 
 Make=$(cat /sys/class/dmi/id/sys_vendor 2> /dev/null)
@@ -145,16 +74,30 @@ fi
 
 log "Updating Kiss"
 
-echo | kiss update
+    kiss update
 
-### Build/Install xorg base
+### Build/Install xorg and desktop applications
 
-for pkg in xorg-server xinit xinput libinput; do
-    log "Building" "$pkg"
-    echo | kiss build $pkg
-    log "Installing" "$pkg"
-    echo | kiss install $pkg
-done
+    kiss b alsa-utils && kiss i alsa-utils
+    kiss b xorg-server && kiss i xorg-server
+    kiss b xf86-input-libinput && kiss i xf86-input-libinput
+    kiss b xauth && kiss i xauth
+    kiss b xclip && kiss i xclip
+    kiss b xset && kiss i xset
+    kiss b xsetroot && kiss i xsetroot
+    kiss b xrandr && kiss i xrandr
+    kiss b xinit && kiss i xinit
+    kiss b hsetroot && kiss i hsetroot
+    kiss b fontconfig && kiss i fontconfig
+    kiss b liberation-fonts && kiss i liberation-fonts
+    kiss b terminus-font && kiss i terminus-font
+    kiss b imagemagick && kiss i imagemagick
+    kiss b firefox-bin && kiss i firefox-bin
+    kiss b dmenu && kiss i dmenu
+    kiss b st && kiss i st
+    kiss b slock && kiss i slock
+    kiss b sxiv && kiss i sxiv
+    kiss b dwm && kiss i dwm
 
 
 ### Build/Install xorg drivers
@@ -163,123 +106,14 @@ if [ "$IsVM" != "true" ]
 
 then
 
-    for pkg in xf86-video-amdgpu xf86-video-intel xf86-video-nouveau xf86-video-vesa xf86-input-libinput; do
-        log "Building" "$pkg"
-        echo | kiss build $pkg
-        log "Installing" "$pkg"
-        echo | kiss install $pkg
-    done
+    kiss b xf86-video-amdgpu && kiss i xf86-video-amdgpu
+    kiss b xf86-video-nouveau && kiss i xf86-video-nouveau
+    kiss b xf86-video-vesa && kiss i xf86-video-vesa
 
 else
 
-    for pkg in xf86-video-vesa xf86-input-libinput; do
-        log "Building" "$pkg"
-        echo | kiss build $pkg
-        log "Installing" "$pkg"
-        echo | kiss install $pkg
-    done
+    kiss b xf86-video-vesa && kiss i xf86-video-vesa
 
 fi
 
-
-### Build/Install xorg utils
-
-for pkg in xset xsetroot xclip fontconfig xrandr; do
-    log "Building" "$pkg"
-    echo | kiss build $pkg
-    log "Installing" "$pkg"
-    echo | kiss install $pkg
-done
-
-
-### Build/Install xorg fonts
-
-for pkg in liberation-fonts terminus-font; do
-    log "Building" "$pkg"
-    echo | kiss build $pkg
-    log "Installing" "$pkg"
-    echo | kiss install $pkg
-done
-
-
-### Build/Install xorg drivers
-
-if [ $IsVM != "true" ]
-then
-
-    for pkg in alsa-lib alsa-utils mesa libdrm intel-vaapi-driver mpv gstreamer gst-plugins gst-plugins-base; do
-        log "Building" "$pkg"
-        echo | kiss build $pkg
-        log "Installing" "$pkg"
-        echo | kiss install $pkg
-    done
-
-fi
-
-
-### Build/Install bash
-
-for pkg in bash; do
-    log "Building" "$pkg"
-    echo | kiss build $pkg
-    log "Installing" "$pkg"
-    echo | kiss install $pkg
-done
-
-
-### Build/Install window manager
-
-for pkg in dwm dmenu slock st rxvt-unicode; do
-    log "Building" "$pkg"
-    echo | kiss build $pkg
-    log "Installing" "$pkg"
-    echo | kiss install $pkg
-done
-
-
-### Build/Install community misc
-
-for pkg in htop pfetch imagemagick sxiv ranger w3m; do
-    log "Building" "$pkg"
-    echo | kiss build $pkg
-    log "Installing" "$pkg"
-    echo | kiss install $pkg
-done
-
-
-### Install firefox-bin
-
-for pkg in firefox-bin; do
-    log "Building" "$pkg"
-    echo | kiss build $pkg
-    log "Installing" "$pkg"
-    echo | kiss install $pkg
-done
-
-
-### Download/extract dotfiles
-
-wget -P /tmp $urlinstallfiles/dotfiles.tar.gz
-
-tar -xvzf /tmp/dotfiles.tar.gz -C /
-
-log "Downloading dotfiles" "${urlinstallfiles}/dotfiles.tar.gz"
-echo ""
-downloadSource "/tmp" "$urlkernel" || war "$?" "Failed to download dotfiles" "${urlinstallfiles}/dotfiles.tar.gz"
-
-log "Extracting dotfiles" "/tmp/dotfiles.tar.gz"
-echo ""
-tar -xvzf /tmp/dotfiles.tar.gz -C / || war "$?" "Failed to extract dotfiles" "/tmp/dotfiles.tar.gz"
-
-
-### Add default user
-
-adduser -k /etc/skel -s /bin/bash user || die "$?" "Failed to add default user"
-
-
-### Add default user to groups
-
-addgroup user video || war "$?" "Failed to add default user to the following group" "video"
-addgroup user audio || war "$?" "Failed to add default user to the following group" "audio"
-addgroup user input || war "$?" "Failed to add default user to the following group" "input"
-addgroup user wheel || war "$?" "Failed to add default user to the following group" "wheel"
+export KISS_PROMPT=1
