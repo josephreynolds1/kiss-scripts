e51eb50f6a147830458e372d222054bcb6580d5e
user <user@laptop1.colossus.home>
2021-03-18 18:26:33 -0400

    Corrected variable spacing

--- os-bootstrap/source/phase1.sh
+++ os-bootstrap/source/phase1.sh
@@ -56,7 +56,7 @@ export MAKEFLAGS="-j${cpucount}"
 
 ### Set url variables
 
-export urlfirmware = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git"
+export urlfirmware="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git"
 
 
 ### Functions #################################################################
